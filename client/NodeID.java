c2e1055f9119f05a0398f9b00a7fd7b233b81436
jeffyPong <jefferson.zou@yale.edu>
2016-05-02 17:47:02 -0400

    Basic server and seed on server side

---
+++ client/NodeID.java
@@ -0,0 +1,12 @@
+//NodeID.java
+//contains InetAddress and Port for given node
+
+public class NodeID {
+	public InetAddress ip;
+	public int port;
+
+	public NodeID(InetAddress ip, int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+}
---
+++ server/TrackerServer.java
@@ -0,0 +1,70 @@
+//details of Tracker Protocol
+
+//Client GET request to server:
+//info_hash: uniquely identifies the file
+//peer_id: chosen by and uniquely identifies the client
+//client IP and port
+//numwant: how many peers to return
+//stats: e.g. bytes uploaded, downloaded
+
+//Tracker GET response to client:
+//interval: how often to contact the tracker
+//list of peers: containing peer id, IP and port
+//stats
+
+import java.util.*;
+import java.net.*;
+
+public class TrackerServer {
+
+	private static ServerSocket welcomeSocket;
+
+	private static String enc = "US-ASCII";
+
+	protected static int threadPoolSize = 5;
+	protected static int serverPort = 6789;
+
+	protected static HashMap<String,ArrayList<InetAddress>> map;
+
+	public static void main(String[] args) throws Exception {
+		serverPort = Integer.parseInt(args[1]);
+		StartSequentialServer();
+	}	
+
+    private static void StartSequentialServer() throws Exception {
+        while(true) {
+            // accept connection from connection queue
+            Socket connectionSocket = welcomeSocket.accept();
+            
+           	BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connSocket.getInputStream(), enc));
+            String line = inFromClient.readLine();
+
+	        while (!(line == "")) {
+	        	//SEED request
+	        	String[] pair = line.split(" ");
+	        	if(pairs[0].equals("SEED")) {
+	        		//add node's InetAddress
+	        		ArrayList<NodeID> list = map.get(pairs[1]);
+	        		NodeID id = new NodeID(connectionSocket.getInetAddress(), connectionSocket.getPort());
+	        		if(list == null) {
+	        			list = new ArrayList<NodeID>();
+	        			list.add(id);
+						map.put(pairs[1],list);
+	        		}
+	        		else {
+	        			list.add(id);
+	        		}
+	        	}
+		        line = inFromClient.readLine();
+	        }
+        }
+    }
+}
+
+
+
+
+
+
+
+
