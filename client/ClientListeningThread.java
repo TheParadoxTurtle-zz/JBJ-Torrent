ba618a0d43d75796784f568c1518ef2280c475fd
jeffyPong <jefferson.zou@yale.edu>
2016-05-03 16:38:03 -0400

    Add file lengths and some client listening functionality

--- client/ClientListeningThread.java
+++ client/ClientListeningThread.java
@@ -32,31 +32,49 @@ public class ClientListeningThread implements Runnable {
 				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connSocket.getInputStream(), Constants.STR_ENCODING));
 	            String line = inFromClient.readLine();
 	            Debug.print(line);
-		        while (!(line.equals(""))) {
-	                StringTokenizer st = new StringTokenizer(line);
-	                String command = st.nextToken();
-	                String arg = st.nextToken();
+	                
+                StringTokenizer st = new StringTokenizer(line);
+                String command = st.nextToken();
+                String arg = st.nextToken();
 
-			        	
-		        	//actually handle commands
-		        	//send bitmap
-		        	if(command.equals("CONNECT")) {
-		        		boolean[] bitmap = new boolean[5];
-		        		//boolean[] bitmap = node.getBitmap();
-		        		StringBuilder buf = new StringBuilder();
-		        		for(int i = 0; i < bitmap.length; i++) {
-		        			if(bitmap[i]) {
-		        				buf.append("1");
-		        			}
-		        			else {
-		        				buf.append("0");
-		        			}
-		        			buf.append("\r\n\r\n");
-		        		}
-		        	}	
+		        String message = "";
 
-			        line = inFromClient.readLine();
-		        }
+	        	//actually handle commands
+	        	//send bitmap
+	        	if(command.equals("CONNECT")) {
+	        		boolean[] bitmap = node.getBitmap(arg);
+	        		StringBuilder buf = new StringBuilder();
+	        		for(int i = 0; i < bitmap.length; i++) {
+	        			if(bitmap[i]) {
+	        				buf.append("1");
+	        			}
+	        			else {
+	        				buf.append("0");
+	        			}
+	        			buf.append("\r\n\r\n");
+	        			message = buf.toString();
+	        		}
+	        	}
+	        	//peer is telling me he has another piece
+	        	else if(command.equals("HAVE")) {
+	        		//get port number
+	        		int port = Integer.parseInt(st.nextToken());
+	        		//get piece number
+	        		int index = Integer.parseInt(st.nextToken());
+	        		//node.updateBitmaps(arg, new NodeID(connSocket.getInetAddress(),port), index);
+	        	}
+	        	//peer is asking if they can request a piece
+	        	else if(command.equals("INTERESTED")) {
+	        		int port = Integer.parseInt(st.nextToken());
+	        		//choose whether to unchoke or choke
+	        		//message = "UNCHOKE";
+	        		//message = "CHOKE"
+	        		message += "\r\n";
+	        	}
+	        	DataOutputStream outToClient = new DataOutputStream(connSocket.getOutputStream());
+		        System.out.println(message);
+		        outToClient.write(message.getBytes("US-ASCII"));
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
