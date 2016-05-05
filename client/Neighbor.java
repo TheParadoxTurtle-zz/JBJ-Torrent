6c6da0b45b2a67ea10113a0cb5dc2917fbc4868e
benftw7 <benftw7@gmail.com>
2016-05-05 19:19:07 -0400

    Neighbor variables

--- client/Neighbor.java
+++ client/Neighbor.java
@@ -3,6 +3,11 @@ package client;
 public class Neighbor {
 	public NodeID nodeid;
 	public boolean bitmap[];
+	public boolean interested_in_them = false;
+	public boolean interested_in_us = false;
+	public boolean unchoked_to_them = false;
+	public boolean unchoked_to_us = false;
+	
 	
 	public Neighbor(NodeID nodeid) {
 		this.nodeid = nodeid;
